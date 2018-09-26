84e1582f296e9c9d2e3ebb29da16938333d3a255
Flavio <flavio.a.618@gmail.com>
2018-09-26 12:32:41 +0200

    Fixed migration to SQLalchemy

--- db_interface.py
+++ db_interface.py
@@ -1,5 +1,5 @@
 import psycopg2
-import sqlalchemy
+import sqlalchemy, sqlalchemy.ext, sqlalchemy.ext.declarative
 from sqlalchemy import Column
 import datetime
 
@@ -29,18 +29,18 @@ class PlayerMatch(Base):
 	id = Column(sqlalchemy.Integer, primary_key = True, nullable = False)
 	id_player = Column(
 		sqlalchemy.Integer,
-		nullable = False,
-		ForeignKey("players.id")
+		sqlalchemy.ForeignKey("players.id"),
+		nullable = False
 	)
 	id_match = Column(
 		sqlalchemy.Integer,
-		nullable = False,
-		ForeignKey("matches.id")
+		sqlalchemy.ForeignKey("matches.id"),
+		nullable = False
 	)
 	total_score = Column(sqlalchemy.Integer)
 
 	__table_args__ = (
-        UniqueConstraint('id_player', 'id_match')
+        sqlalchemy.UniqueConstraint('id_player', 'id_match'),
     )
 
 
@@ -96,150 +96,167 @@ class DBInterface:
 
 	# Loads whitelist from the DB (adding each name saved)
 	def loadWithelist(self):
-		with sqlalchemy.orm.sessionmaker(bind = self.engine) as session:
-			self.whitelist = [x.name.lower() for x in session.query(Player)]
+		session = sqlalchemy.orm.sessionmaker(bind = self.engine)()
+		self.whitelist = [x[0] for x in session.query(Player.name).all()]
 
 
 	# Adds a player to the DB, only if they're in withelist
-	def createPlayer(self, name):
+	def createPlayer(self, name, **kwargs):
 		if name.lower() not in self.whitelist:
 			return
-		with sqlalchemy.orm.sessionmaker(bind = self.engine) as session:
-			session.add(Player(name = name.lower()))
+		if 'session' in kwargs:
+			session = kwargs['session']
+		else:
+			session = sqlalchemy.orm.sessionmaker(bind = self.engine)()
+		session.add(Player(name = name.lower()))
+		if 'session' not in kwargs:
+			session.commit()
 
 	# Gets the ID of a player from its name
-	def getPlayerId(self, name):
-		with sqlalchemy.orm.sessionmaker(bind = self.engine) as session:
-			fetches = session.query(Player).filter_by(name = name)
-			if len(fetches) == 0:
-				return None
-			if len(fetches) > 1:
-				# Should never happen because of uniqueness constraint, is here
-				# just to be sure
-				raise IndexError('Too many matches found with the passed name')
-			return fetches.first().id
+	def getPlayerId(self, name, **kwargs):
+		if 'session' in kwargs:
+			session = kwargs['session']
+		else:
+			session = sqlalchemy.orm.sessionmaker(bind = self.engine)()
+		# Should never find more than one because of uniqueness constraint
+		pl = session.query(Player.id).filter_by(name = name).one_or_none()
+		return pl[0] if pl is not None else pl
 
 	# Given name and surname, returns the player's ID. If they don't exist,
 	# creates them. If name isn't in whitelist, returns None
-	def findOrCreatePlayer(self, name):
+	def findOrCreatePlayer(self, name, **kwargs):
 		if name.lower() not in self.whitelist:
 			return None
-		player_id = self.getPlayerId(name)
+		player_id = self.getPlayerId(name, session = kwargs['session'])
 		if player_id:
 			return player_id
-		self.createPlayer(name)
-		return self.getPlayerId(name)
+		self.createPlayer(name, session = kwargs['session'])
+		return self.getPlayerId(name, session = kwargs['session'])
 
 
 	# Adds a match without a category (that is, map and timelimit)
-	def addEmptyMatch(self, link):
-		with sqlalchemy.orm.sessionmaker(bind = self.engine) as session:
-			session.add(Match(link = link, addtime = datetime.datetime.utcnow()))
+	def addEmptyMatch(self, link, **kwargs):
+		if 'session' in kwargs:
+			session = kwargs['session']
+		else:
+			session = sqlalchemy.orm.sessionmaker(bind = self.engine)()
+		session.add(Match(link = link, addtime = datetime.datetime.utcnow()))
+		if 'session' not in kwargs:
+			session.commit()
 
 	# Gets the ID of a match from its link
-	def getMatchId(self, link):
-		with sqlalchemy.orm.sessionmaker(bind = self.engine) as session:
-			fetches = session.query(Match).filter_by(link = link)
-			if len(fetches) == 0:
-				return None
-			if len(fetches) > 1:
-				# Should never happen because link is unique, is here just to be sure
-				raise IndexError('Too many matches found with the passed link')
-			return fetches.first().id
+	def getMatchId(self, link, **kwargs):
+		if 'session' in kwargs:
+			session = kwargs['session']
+		else:
+			session = sqlalchemy.orm.sessionmaker(bind = self.engine)()
+		# Should never find more than one because of uniqueness constraint
+		mtc = session.query(Match.id).filter_by(link = link).one_or_none()
+		return mtc[0] if mtc is not None else mtc
 
 	# Given link, returns the match's ID. If it doesn't exist, creates it
 	# without category
-	def findOrCreateMatch(self, link):
-		match_id = self.getMatchId(link)
+	def findOrCreateMatch(self, link, **kwargs):
+		match_id = self.getMatchId(link, session = kwargs['session'])
 		if match_id:
 			return match_id
-		self.addEmptyMatch(link)
-		return self.getMatchId(link)
+		self.addEmptyMatch(link, session = kwargs['session'])
+		return self.getMatchId(link, session = kwargs['session'])
 
 
 	# Adds a playerMatch to the DB
-	def createPlayerMatch(self, id_player, id_match, total_score):
-		with sqlalchemy.orm.sessionmaker(bind = self.engine) as session:
-			session.add(PlayerMatch(
-				id_player = id_player,
-				id_match = id_match,
-				total_score = total_score
-			))
+	def createPlayerMatch(self, id_player, id_match, total_score, **kwargs):
+		if 'session' in kwargs:
+			session = kwargs['session']
+		else:
+			session = sqlalchemy.orm.sessionmaker(bind = self.engine)()
+		session.add(PlayerMatch(
+			id_player = id_player,
+			id_match = id_match,
+			total_score = total_score
+		))
+		if 'session' not in kwargs:
+			session.commit()
 
 	# Gets the ID of a player from name and surname
-	def getPlayerMatchId(self, id_player, id_match):
-		with sqlalchemy.orm.sessionmaker(bind = self.engine) as session:
-			fetches = session.query(PlayerMatch).filter_by(
-				id_player = id_player,
-				id_match = id_match
-			)
-			if len(fetches) == 0:
-				return None
-			if len(fetches) > 1:
-				# Should never happen because link is unique, is here just to be sure
-				raise IndexError('Too many matches found with the passed link')
-			return fetches.first().id
+	def getPlayerMatchId(self, id_player, id_match, **kwargs):
+		if 'session' in kwargs:
+			session = kwargs['session']
+		else:
+			session = sqlalchemy.orm.sessionmaker(bind = self.engine)()
+		# Should never find more than one because of uniqueness constraint
+		pm = session.query(PlayerMatch.id).filter_by(
+			id_player = id_player,
+			id_match = id_match
+		).one_or_none()
+		return pm[0] if pm is not None else pm
 
 	# Given name and surname, returns the player's ID. If they don't exist,
 	# creates them
-	def findOrCreatePlayerMatch(self, id_player, id_match, total_score):
-		playerMatch_id = self.getPlayerMatchId(id_player, id_match)
+	def findOrCreatePlayerMatch(self, id_player, id_match, total_score, **kwargs):
+		playerMatch_id = self.getPlayerMatchId(id_player, id_match, session = kwargs['session'])
 		if playerMatch_id:
 			return playerMatch_id
-		self.createPlayerMatch(id_player, id_match, total_score)
-		return self.getPlayerMatchId(id_player, id_match)
+		self.createPlayerMatch(id_player, id_match, total_score, session = kwargs['session'])
+		return self.getPlayerMatchId(id_player, id_match, session = kwargs['session'])
 
 
 	# Gets the list of saved links. If a datetime is passed, returns only
 	# matches more recent than that date
 	def getLinksList(self, since = None):
-		with sqlalchemy.orm.sessionmaker(bind = self.engine) as session:
-			fetches = session.query(Match.link)
-			if since is not None:
-				fetches = fetches.filter(Match.addtime >= since)
-			return fetches
+		session = sqlalchemy.orm.sessionmaker(bind = self.engine)()
+		# Should never find more than one because of uniqueness constraint
+		fetches = session.query(Match.link)
+		if since is not None:
+			fetches = fetches.filter(Match.addtime >= since)
+		return [x[0] for x in fetches]
 
 	# Gets the list of saved id_match for the passed category. If a datetime is
 	# passed, returns only matches more recent than that date
 	def getMatchesList(self, mapType, timelimit, since = None):
-		with sqlalchemy.orm.sessionmaker(bind = self.engine) as session:
-			fetches = session.query(Match.id).filter_by(
-				map = mapType,
-				timelimit = timelimit
-			)
-			if since is not None:
-				fetches = fetches.filter(Match.addtime >= since)
-			return fetches
+		session = sqlalchemy.orm.sessionmaker(bind = self.engine)()
+		fetches = session.query(Match.id).filter_by(
+			map = mapType,
+			timelimit = timelimit
+		)
+		if since is not None:
+			fetches = fetches.filter(Match.addtime >= since)
+		return [x[0] for x in fetches]
 
 	# Gets the list of links of matches that the player 'name' hasn't played
 	def getUnplayedMatchesList(self, name):
-		with sqlalchemy.orm.sessionmaker(bind = self.engine) as session:
-			subquery = session.query(Match.id)
-						.filter(Match.id == PlayerMatch.id_match)
-						.filter(PlayerMatch.id_player == Player.id)
-						.filter(Player.name == name)
-			fetches = session.query(Match.link).filter(~Match.id.in_(subquery))
-			return fetches
+		session = sqlalchemy.orm.sessionmaker(bind = self.engine)()
+		subquery = session.query(Match.id)\
+					.filter(Match.id == PlayerMatch.id_match)\
+					.filter(PlayerMatch.id_player == Player.id)\
+					.filter(Player.name == name)
+		fetches = session.query(Match.link).filter(~Match.id.in_(subquery))
+		return [x[0] for x in fetches]
 
 
 	# Updates a match in the DB, possibly creating any row needed. The first
 	# parameter is the match's link, the second is the json from the page
 	# parsed into a Python dict
 	def updateMatch(self, link, json):
-		with sqlalchemy.orm.sessionmaker(bind = self.engine) as session:
-			this_match = session.query(Match).filter_by(link = link).first()
-			id_match = this_match.id
-			this_match.map = json['mapSlug']
-			this_match.link = json['roundTimeLimit']
+		session = sqlalchemy.orm.sessionmaker(bind = self.engine)()
+		this_match = session.query(Match).filter_by(link = link).first()
+		id_match = this_match.id
+		this_match.map = json['mapSlug']
+		this_match.timelimit = json['roundTimeLimit']
 		# Possibly creates playermatch for each player
 		for player in json['hiScores']:
-			id_player = self.findOrCreatePlayer(player['playerName'])
+			id_player = self.findOrCreatePlayer(
+				player['playerName'],
+				session = session
+			)
 			if id_player is not None:
 				self.findOrCreatePlayerMatch(
 					id_player,
 					id_match,
-					player['totalScore']
+					player['totalScore'],
+					session = session
 				)
+		session.commit()
 
 
 	# Gets the list of pairs (player, score) for a single match from its ID
